720c1ae8aa254b0cc7d9538a044fb83ffa6ff2b1
Artem Abramov <49606840+artabramov@users.noreply.github.com>
2023-12-23 17:16:47 +0400

    Updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-# service cron start
-# service ntp start
+service postgresql start
+service redis-server start
+service cron start
+service ntp start
 uvicorn app.app:app --host 0.0.0.0 --port 8080 --reload
 /usr/sbin/apache2ctl -DFOREGROUND
