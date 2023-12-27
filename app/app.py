91c54d17f166ba756cdb645bacbac13384e5ca13
Artem Abramov <49606840+artabramov@users.noreply.github.com>
2023-12-27 05:32:15 +0000

    Draft commit.

--- app/app.py
+++ app/app.py
@@ -14,6 +14,21 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(user_routers.router)
 
 
+
+from fastapi import FastAPI, Request, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    a = 1
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder({"detail": exc.errors(), "Error": "Name field is missing"}),
+    )
+
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -4,6 +4,11 @@ from app.models.user_meta import UserMeta
 from app.schemas.user_schema import UserInsert #, UserSelect
 from app.managers.entity_manager import EntityManager
 from fastapi import HTTPException
+from fastapi.responses import JSONResponse
+from fastapi import status
+from fastapi.encoders import jsonable_encoder
+from pydantic import ValidationError
+from fastapi.exceptions import RequestValidationError
 
 
 class UserRepository():
@@ -12,8 +17,31 @@ class UserRepository():
         self.entity_manager = entity_manager
 
     def insert(self, schema: UserInsert):
+        # try:
         if self.entity_manager.exists(User, user_login=schema.user_login):
-            raise HTTPException(status_code=418, detail="User login already occupied.")
+            raise RequestValidationError({
+                "type": "value_exists",
+                "loc": ("query", "user_login"),
+                "msg": "The value already exists",
+                "input": schema.user_login,
+            })
+            # raise RequestValidationError(detail="sadf")
+            # return JSONResponse(
+            #     status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            #     content=jsonable_encoder({"detail": "sd", "Error": "Name field is missing"}),
+            # )
+
+            # raise HTTPException(status_code=404, detail="Name field is required")
+            # raise ValueError("fuck!")
+                # return JSONResponse(
+                #     status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                #     content=jsonable_encoder({"detail": "error details", "body": "error body"}),
+                # )
+        # except ValueError as e:
+        #     return JSONResponse(
+        #         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        #         content=jsonable_encoder({"detail": "error details", "body": "error body", "loc": {"ssf": "sdf"}}),
+        #     )
 
         try:
             user = User(user_login=schema.user_login, user_pass=schema.user_pass, first_name=schema.first_name,
