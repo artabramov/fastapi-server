f5820e89014959e6e1802ecd854b72d5b63337fc
Artem Abramov <49606840+artabramov@users.noreply.github.com>
2024-01-07 11:52:13 +0000

    Draft commit.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ service redis-server start
 service cron start
 service ntp start
 service apache2 start
-cd /memo && uvicorn app.app:app --host 0.0.0.0 --port 8080 --reload
+cd /memo && uvicorn app.app:app --host 0.0.0.0 --port 80 --reload
