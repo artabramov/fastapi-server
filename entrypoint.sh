c0aa0a137d20be2249004282600e5b36ee353800
Artem Abramov <49606840+artabramov@users.noreply.github.com>
2024-01-07 10:06:39 +0000

    Draft commit.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ service redis-server start
 service cron start
 service ntp start
 service apache2 start
-cd /memo && uvicorn app.app:app --host 127.0.0.1 --port 8080 --reload
+cd /memo && uvicorn app.app:app --host 0.0.0.0 --port 8080 --reload
 /usr/sbin/apache2ctl -DFOREGROUND
--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
 docker build --no-cache -t memo-server .
-docker run -dit -p 80:8080 -p 5432:5432 --name memo-server memo-server
+docker run -dit -p 80:8080 -p 8081:8081 -p 5432:5432 --name memo-server memo-server
 docker exec memo-server sudo -u postgres psql -c "CREATE USER memo WITH PASSWORD 'he7w2rLY4Y8pFk2u';"
 docker exec memo-server sudo -u postgres psql -c "CREATE DATABASE memo;"
