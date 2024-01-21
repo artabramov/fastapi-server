7c97ceec05d3332876a0fa882163037302d85c42
Artem Abramov <49606840+artabramov@users.noreply.github.com>
2024-01-21 12:26:59 +0000

    Draft commit.

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 docker rm media-server
 docker rmi media-server
 docker build --no-cache -t media-server .
-docker run -dit -p 80:80 -p 8081:8081 -p 5432:5432 --name media-server media-server
+# docker run -dit -p 80:80 -p 8081:8081 -p 5432:5432 --name media-server media-server
+
+docker-compose up -d
 docker exec media-server sudo -u postgres psql -c "CREATE USER media WITH PASSWORD 'he7w2rLY4Y8pFk2u';"
 docker exec media-server sudo -u postgres psql -c "CREATE DATABASE media;"
